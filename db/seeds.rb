6ccb9cc7ba94439a7b52b7b008745aca54958a11
Vitor Dal Pra <vitordalpra@gmail.com>
2020-11-17 16:52:00 -0300

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating 10 posts...'
+10.times do
+  article = Article.new(
+    title: "#{Faker::Movies::StarWars.character} by a Wookiee",
+    content: "#{Faker::Movies::StarWars.wookiee_sentence}"
+  )
+  article.save!
+end
+puts 'Finished!'
